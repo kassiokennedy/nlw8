b5a84b0f674de35221eaca924b410291b19a905d
kassio kennedy <kassio1206@hotmail.com>
2022-05-22 20:48:18 -0300

    video 1 ok

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,29 +1,14 @@
 //componente é uma função que retorna um HTML
-//função que define as caracteristicas da função Button
-interface ButtonProps{
-  text: string;
 
-}
-//função que define o componente
-function Button(props: ButtonProps){
-  return <button className="bg-violet-500 p-2">{props.text}</button>
-}
+import { Widget } from "./components/Widget";
 
 
-function App() {
+export function App() {
   /*
    a div chama diversos componentes
   o react espera um unico componente, no caso a div,
   que dentro dela pode ser colocada uma quantidade maior coisas
   
   */
-   return (
-    <div>
-      <Button text="Enviar"/>
-      <Button text="Ok"/>
-
-    </div>
-  )
+   return <Widget />
 }
-
-export default App
